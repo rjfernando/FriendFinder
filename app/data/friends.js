07a84f86025dc926aa5b777229267752b9ec5162
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-03-02 21:29:47 -0800

    adding names to friends file

---
+++ app/data/friends.js
@@ -0,0 +1,163 @@
+const friends= [
+
+{
+    "name": "Micheal Jordan",
+    "photo": "https://cdn.nba.net/nba-drupal-prod/styles/landscape/http/i2.cdn.turner.com/nba/nba/dam/assets/130828123801-michael-jordan-iso-1998-all-star-game.video-player.jpg?itok=FGvQfRCL",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Larry Bird",
+    "photo": "https://vignette.wikia.nocookie.net/en.futurama/images/c/c9/Larry_Bird.jpg/revision/latest?cb=20171023032625",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Chris Webber",
+    "photo": "https://images-na.ssl-images-amazon.com/images/I/414dGsjEXhL.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},{
+    "name": "Tim Hardway",
+    "photo": "https://i.ytimg.com/vi/1oBnxQUgVKc/maxresdefault.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Kevin Durant",
+    "photo": "https://www.gannett-cdn.com/-mm-/df8c98f1a21fcb1ddabf7b281d5a71eb6e08c39f/c=32-152-659-988&r=537&c=0-0-534-712/local/-/media/2017/04/09/USATODAY/USATODAY/636273011086129297-USP-NBA-NEW-ORLEANS-PELICANS-AT-GOLDEN-STATE-WARR-90131784.JPG",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Stephen Curry",
+    "photo": "http://media.breitbart.com/media/2017/11/stephen-curry-ap-photo-640x480.png",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Kobe Bryant",
+    "photo": "http://www.trbimg.com/img-52a506f3/turbine/lat-sp-kobe-bryant-photo-20131208/300/300x169",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Lebron James",
+    "photo": "https://mk0slamonlinensgt39k.kinstacdn.com/wp-content/uploads/2017/11/GettyImages-874698670.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Russell Westbrook",
+    "photo": "https://clutchpoints.com/wp-content/uploads/2017/10/f51e615d782447bb99b59ca56290e6f7-e1509252319914.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Joe Montana",
+    "photo": "http://static.nfl.com/static/content/public/pg-photo/2008/04/09/09000d5d807ab819_gallery_600.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}];
+
+module.exports = friends;
