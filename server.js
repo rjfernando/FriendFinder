b6eb0f732245adbdf6f084949bfb0a17e51254ff
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-03-04 21:07:52 -0800

    server.js update

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyparser = require('body-parser');
+const path = require('path');
+//establishes the express app and port for server
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+//the express app to handle data parsing
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+//routes for app
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//starts the server to start listing to the designated PORT
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
